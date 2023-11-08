e9dd3e10be6525df8b585b652a82934904f52762
Vitaly Tikhoplav <tikhoplavspb@gmail.com>
2023-11-08 17:15:31 +0200

    added bit implementation

---
+++ benches/byte_state_update.rs
@@ -0,0 +1,23 @@
+use conway::byte_state::{new, random, tick, State};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn byte_state_update_bench(c: &mut Criterion) {
+    // sha256 "Hello, World!!"
+    let seed: [u8; 32] = [
+        3, 127, 146, 120, 48, 195, 83, 11, 249, 175, 8, 97, 14, 74, 238, 206, 54, 140, 118, 249,
+        190, 114, 71, 9, 151, 44, 72, 54, 15, 171, 30, 139,
+    ];
+
+    let mut even: Box<State<1024>> = black_box(Box::new(random(seed)));
+    let mut odd: Box<State<1024>> = black_box(Box::new(new()));
+
+    c.bench_function("byte_state_update", |b| {
+        b.iter(|| {
+            tick(&even, &mut odd);
+            tick(&odd, &mut even);
+        })
+    });
+}
+
+criterion_group!(bench, byte_state_update_bench);
+criterion_main!(bench);
